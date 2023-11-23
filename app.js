496dcba3709049c1a3cd971396e5d8ebcd372540
your_name_here <rautsagar214@gmail.com>
2023-11-23 18:23:00 +0530

    day4

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ window.location.href="./second.html"
 return false
 }
 
+window.addEventListener("DOMContentLoaded",()=>{
+    var backGroundMusic = document.getElementById("background-music")
+    Audio.volume=0.5
+    Audio.loop=true;
+    
+
+})
+ 
+var start=document.getElementById("start")
+start.onclick=()=>{
+var audio=new Audio("./button-8-88355.mp3")
+audio.play()
+}
+ 
 
 // startButton.addEventListener("click",()=>{
 //     if (textbox.value==""){
@@ -23,9 +37,14 @@ return false
 //     else{localStorage.setItem("name",textbox.value)
 //     window.location.href="./second.html"}
 // })
-startButton.onclick = ()=>{
-    window.location.href="./second.html"
-}
+// startButton.onclick = ()=>{
+//     window.location.href="./second.html"
+// }
 
+// var backGroundMusic = new Audio(`Guess-the-number-game\Ukulele-and-Piano(chosic.com).mp3`);
 
+// backGroundMusic.autoplay=true;
+// backGroundMusic.loop=true;
+
+// document.body.appendChild(backGroundMusic);
 
